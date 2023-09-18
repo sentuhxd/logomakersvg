1d999feec09ad4b142de2885eead037a812d5cd5
sentuh <142236429+sentuhxd@users.noreply.github.com>
2023-09-18 14:10:39 -0400

    added base shapejs class

---
+++ libs/shape.js
@@ -0,0 +1,9 @@
+class Shape {
+    constructor(shapeColor, textColor, text) {
+        this.shapeColor = shapeColor;
+        this.textColor = textColor;
+        this.text = text;
+    }
+};
+
+module.exports = Shape;
