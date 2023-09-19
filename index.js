6def92c4df4049f0bb7e888cf0d38a9e35575322
sentuh <142236429+sentuhxd@users.noreply.github.com>
2023-09-19 14:39:45 -0400

    added start function

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
 const inquirer = require('inquirer');
 const fs = require('fs')
 const {writeFile} = require('fs/promises')
-const svg = require('./libs/svg.js')
+const Svg = require('./libs/svg.js')
 const {Square, Circle, Triangle} = require('./libs/shape.js')
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'logoName',
-            message: 'Enter 1-3 Letters for Your Logo',
-        },
-        {
-            type: 'input',
-            name: 'logoColor',
-            message: 'Enter a colorkeyword or HEXADECIMAL number for the logos background color',
-        },
-        {
-            type: 'list',
-            name: 'textColor',
-            message: 'Enter a colorkeyword or HEXADECIMAL number for the logos text color',
-        },
-        {
-            type: 'input',
-            name: 'logoShape',
-            message: 'Choose a Logo Shape',
-            choices: ['square', 'circle', 'triangle'],
+function start() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'logoName',
+                message: 'Enter 1-3 Letters for Your Logo',
+            },
+            {
+                type: 'input',
+                name: 'logoColor',
+                message: 'Enter a colorkeyword or HEXADECIMAL number for the logos background color',
+            },
+            {
+                type: 'list',
+                name: 'textColor',
+                message: 'Enter a colorkeyword or HEXADECIMAL number for the logos text color',
+            },
+            {
+                type: 'input',
+                name: 'logoShape',
+                message: 'Choose a Logo Shape',
+                choices: ['square', 'circle', 'triangle'],
 
-        },
+            },
     ])
-    .then((data) =>{
-        const pathSVG = './dist/logo.svg';
-        const logoFinal = genShape(data);
+    .then((responses) => {
+        if(responses.text.length > 3) {
+            console.log('Keep it 3 Letters or Under');
+            start();
+        } else {
+            writetoFile('logo.svg', responses);
+        }
+    });
 
-        fs.writeFile(pathSVG, )
-    })
+}
+
+start();
--- libs/shape.js
+++ libs/shape.js
@@ -10,19 +10,19 @@ class Shape {
 
 class Square extends Shape {
     render() {
-        return `<rect x="90" y="40" width="120" height="120" fill="${this.shapeColor}" />`
+        return `<rect x="73" y="40" width="160" height="160" fill="${this.color}" />`;
     }
 }
 
 class Triangle extends Shape {
     render() {
-        return `<circle cx="150" cy="100" r="80" fill="${this.shapeColor}" />`
+        return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />`;
     }
 }
 
 class Circle extends Shape {
     render() {
-        return `<circle cx="150" cy="100" r="80" fill="${this.shapeColor}" />`
+        return `<circle cx="150" cy="115" r="80" fill="${this.color}" />`;
     }
 }
 module.exports = {Square, Triangle, Circle};
